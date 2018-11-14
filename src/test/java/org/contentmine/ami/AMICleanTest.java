45c97bb3a471cb2206b64c5f2ff89427ad506491
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2018-11-14 14:34:52 +0000

    adding docs

--- src/test/java/org/contentmine/ami/AMICleanTest.java
+++ src/test/java/org/contentmine/ami/AMICleanTest.java
@@ -2,13 +2,18 @@ package org.contentmine.ami;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.contentmine.ami.AMICleaner.Cleaner;
 import org.contentmine.cproject.files.CProject;
+import org.contentmine.cproject.files.DebugPrint;
+import org.contentmine.cproject.util.CMineGlobber;
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 /** test cleaning.
  * 
@@ -27,8 +32,17 @@ public class AMICleanTest {
 		FileUtils.copyDirectory(AMIFixtures.TEST_BMC_DIR, destDir);
 		CProject cProject = new CProject(destDir);
 		AMICleaner cleaner = new AMICleaner(cProject);
+		CMineGlobber globber = new CMineGlobber().setRegex(".*/scholarly\\.html").setLocation(destDir);
+		List<File> fileList = globber.listFiles();
+		Assert.assertEquals(4,  fileList.size());
 		cleaner.clean("junk");
+		fileList = globber.listFiles();
+		Assert.assertEquals(4,  fileList.size());
 		cleaner.clean(Cleaner.SCHOLARLY_HTML.file);
+		fileList = globber.listFiles();
+		DebugPrint.debugPrint("files "+fileList);
+		
+		Assert.assertEquals(1,  fileList.size());
 		
 	}
 }
